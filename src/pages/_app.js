41773064e2ff572a3eb6e8743cd7b7e912412f7b
luageo <ldeoliveiratech@gmail.com>
2023-06-15 16:16:03 +0200

    list of movies added to Home

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
+import { Header } from "@/components/Header";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Hollywood Boulevard</title>
+      </Head>
+      <Head>
+        <title>Hollywood Boulevard</title>
+      </Head>
+      <Component {...pageProps} />
+      <Header />
+    </>
+  );
 }
